b17d023cbdb2e22f89bc6ecabe863a88d4ba903f
LoganMinarik <168377908+LoganMinarik@users.noreply.github.com>
2025-01-08 11:48:29 -0500

    .

---
+++ workspace/cstar/src/main.c
@@ -0,0 +1,1 @@
+///I so this is where the base code will go which is like normal C/C++ but its going to be an interpeter rather than a complier, yes it will be slower but i wil add python level error reports and detections
