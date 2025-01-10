5888164adb28c61bc346e4c0035ceb75ad6b4251
LoganMinarik <168377908+LoganMinarik@users.noreply.github.com>
2025-01-09 19:30:22 -0500

    first succsess

--- workspace/cstar/src/main.c
+++ workspace/cstar/src/main.c
@@ -1,1 +1,10 @@
 ///I so this is where the base code will go which is like normal C/C++ but its going to be an interpeter rather than a complier, yes it will be slower but i wil add python level error reports and detections
+#include <stdio.h>
+
+int main (int argc, char* argv[])
+{
+    printf("Hello World!\n");
+
+    return 0;
+}
+
